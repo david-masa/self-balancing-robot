410bf8bfda90a3cd328f60e02e336d681a77692a
DavidMasa <apple.phone.3g.masa@gmail.com>
2026-09-23 16:40:45 +0900

    TB6612FNG　ピン番号定義

--- main/main.c
+++ main/main.c
@@ -2,10 +2,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void app_main(void)
-{
-    printf("Hello, ESP32-S3!\n");
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
+#define PWMA_PIN    GPIO_NUM_1              //orange
+#define AIN2_PIN    GPIO_NUM_2              //blue
+#define AIN1_PIN    GPIO_NUM_42             //gray
+
+#define STBY_PIN    GPIO_NUM_41             //green
+
+#define BIN1_PIN    GPIO_NUM_40             //white
+#define BIN2_PIN    GPIO_NUM_39             //purple
+#define PWMB_PIN    GPIO_NUM_38             //yellow
