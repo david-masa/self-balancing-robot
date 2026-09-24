bcb453540dc403bf3ce200e0028d907527de3dbf
DavidMasa <apple.phone.3g.masa@gmail.com>
2026-09-24 20:17:37 +0900

    ピンアサイン変更

--- main/main.c
+++ main/main.c
@@ -3,12 +3,12 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define PWMA_PIN    GPIO_NUM_1              //orange
-#define AIN2_PIN    GPIO_NUM_2              //blue
-#define AIN1_PIN    GPIO_NUM_42             //gray
+#define PWMA_PIN    GPIO_NUM_4             //orange
+#define AIN2_PIN    GPIO_NUM_5             //blue
+#define AIN1_PIN    GPIO_NUM_6             //gray
 
-#define STBY_PIN    GPIO_NUM_41             //green
+#define STBY_PIN    GPIO_NUM_7              //green
 
-#define BIN1_PIN    GPIO_NUM_40             //white
-#define BIN2_PIN    GPIO_NUM_39             //purple
-#define PWMB_PIN    GPIO_NUM_38             //yellow
+#define BIN1_PIN    GPIO_NUM_15             //white
+#define BIN2_PIN    GPIO_NUM_16             //purple
+#define PWMB_PIN    GPIO_NUM_17             //yellow
