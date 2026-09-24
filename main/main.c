53c95526a27d573eb624fea7331c729b0d6cdd7d
DavidMasa <apple.phone.3g.masa@gmail.com>
2026-09-25 06:21:46 +0900

    Update main.c

--- main/main.c
+++ main/main.c
@@ -3,12 +3,12 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define PWMA_PIN    GPIO_NUM_4             //orange
-#define AIN2_PIN    GPIO_NUM_5             //blue
-#define AIN1_PIN    GPIO_NUM_6             //gray
+#define PWMA_PIN    GPIO_NUM_4           　  //orange
+#define AIN2_PIN    GPIO_NUM_5          　   //blue
+#define AIN1_PIN    GPIO_NUM_6           　  //gray
 
-#define STBY_PIN    GPIO_NUM_7              //green
+#define STBY_PIN    GPIO_NUM_7           　  //green
 
-#define BIN1_PIN    GPIO_NUM_15             //white
+#define BIN1_PIN    GPIO_NUM_15         　   //white
 #define BIN2_PIN    GPIO_NUM_16             //purple
 #define PWMB_PIN    GPIO_NUM_17             //yellow
